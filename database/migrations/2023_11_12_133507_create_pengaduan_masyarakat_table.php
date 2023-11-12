a3dafdce231f77a3fde465b7d57c3c00476c185c
hadiprasetiyo <hadiiyok01@gmail.com>
2023-11-12 23:38:37 +0800

    feat: migration tabel users, masyarakat, pengaduan_fasilitas, tanggapan

---
+++ database/migrations/2023_11_12_133507_create_pengaduan_masyarakat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengaduan_masyarakat', function (Blueprint $table) {
+          $table->unsignedBigInteger('id')->primary();
+          $table->date('tgl_pengaduan');
+          $table->text('deskripsi');
+          $table->text('lokasi_pengaduan');
+          $table->string('foto_pengaduan', 45);
+          $table->enum('status', ['Belum diproses', 'Proses', 'Selesai'])->default('Belum diproses');
+          $table->integer('masyarakat_id')->unsigned();
+          $table->foreign('masyarakat_id')->references('id')->on('masyarakat');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengaduan_masyarakat');
+    }
+};
