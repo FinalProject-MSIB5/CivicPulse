3a9c6ae243638d2cf4fefffc059e8b728c002a46
hadiprasetiyo <hadiiyok01@gmail.com>
2023-12-10 21:20:34 +0800

    feat(controller/api): membuat controller API Data User. Create,Read,Update pada DataUserController

--- app/Http/Controllers/Api/DataUserController.php
+++ app/Http/Controllers/Api/DataUserController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DataUserResource;
+use App\Models\Masyarakat;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class DataUserController extends Controller
@@ -19,6 +19,7 @@ class DataUserController extends Controller
     return new DataUserResource(true,'Semua Data User',$users);
   }
 
+  // Membuat Data user
   public function create(Request $request)
   {
     $validator = Validator::make($request->all(),[
@@ -47,14 +48,14 @@ class DataUserController extends Controller
       $nama = Str::random(5);
       $extensi = $request->foto->extension();
       $fileName =  $nama . '.' .  $extensi;
-      //$fileName = $request->foto->getClientOriginalName();
       $request->foto->move(public_path('assets/img/profile'),$fileName);
     }
     else{
         $fileName = '';
     }
 
-    DB::table('masyarakat')->insert([
+    DB::table('masyarakat')->insert(
+      [
         'user_id' => $user->id,
         'nik'=>$request->nik,
         'no_telepon'=>$request->no_telepon,
@@ -62,11 +63,10 @@ class DataUserController extends Controller
         'gender'=>$request->gender,
         'foto'=>$fileName,
       ]);
-     
-      //pengkondisian postman 
+
       if ($user) {
         return new DataUserResource(true,'Data berhasil di input',$user);
-    } else {
+      } else {
       return response()->json(
         [
           'success'=>false,
@@ -77,11 +77,7 @@ class DataUserController extends Controller
     }
   }
 
-  public function store(Request $request)
-  {
-    
-  }
-
+  // Menampilkan Data User
   public function show($id)
   {
     $user = User::find($id);
@@ -99,74 +95,33 @@ class DataUserController extends Controller
     }
   }
 
-  public function edit(string $id)
-  {
-    
-  }
-
+  // Mengubah Data User
   public function update(Request $request, string $id)
   {
-    $validator = Validator::make($request->all(), [
-      'id' => 'required|max:45',
+    $validator = Validator::make($request->all(),
+    [
       'nama' => 'required|max:45',
-      'email' => 'required|email',
-      'password' => 'required|max:45',
       'nik' => 'required|min:16',
       'no_telepon' => 'required|max:12',
       'alamat' => 'required|max:100',
       'gender' => 'required|max:45',
-      'foto' => 'required|image|mimes:jpg,png,jpeg|max:2048'
-  ]);
-  
-  if ($validator->fails()) {
-      return response()->json($validator->errors(), 422);
-  }
-  
-  // Ambil user yang akan diupdate
-  $user = User::find($request->input('id'));
-  
-  // Perbarui data user
-  $user->update([
-      'nama' => $request->nama,
-      'email' => $request->email,
-      'password' => $request->password
-  ]);
-  
-  // Perbarui data masyarakat
-  if (!empty($request->foto)) {
-      $nama = Str::random(5);
-      $extensi = $request->foto->extension();
-      $fileName = $nama . '.' . $extensi;
-      $request->foto->move(public_path('assets/img/profile'), $fileName);
-  } else {
-      $fileName = '';
-  }
-  
-  DB::table('masyarakat')->where('user_id', $user->id)->update([
-      'nik' => $request->nik,
-      'no_telepon' => $request->no_telepon,
-      'alamat' => $request->alamat,
-      'gender' => $request->gender,
-      'foto' => $fileName,
-  ]);
-  
-  // Pengkondisian Postman
-  if ($user) {
-      return new DataUserResource(true, 'Data berhasil diupdate', $user);
-  } else {
-      return response()->json(
-          [
-              'success' => false,
-              'message' => 'Data tidak dapat diupdate',
-              'data' => $user
-          ], 404
-      );
-  }
-  
-  }
+    ]);
 
-  public function destroy(string $id)
-  {
-    
+    if ($validator->fails()) {
+      return response()->json(['error' => $validator->errors()], 422);
+    }
+
+    DB::table('users')->where('id', $id)->update([
+      'nama' => $request->input('nama'),
+    ]);
+
+    $masyarakat = Masyarakat::whereId($id)->update(
+    [
+      'nik' => $request->input('nik'),
+      'no_telepon' => $request->input('no_telepon'),
+      'alamat' => $request->input('alamat'),
+      'gender' => $request->input('gender'),
+    ]);
+    return new DataUserResource(true,'Data Berhasil Diubah',$masyarakat);
   }
 }
